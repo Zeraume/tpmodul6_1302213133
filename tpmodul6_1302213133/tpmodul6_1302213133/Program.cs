b1da362c0a55bd1e32644b07bf7252302b2c9013
Zeraume <farissiddiqrp@gmail.com>
2023-03-27 04:21:56 +0700

    menambahkan class SayaTubeVideo

---
+++ tpmodul6_1302213133/tpmodul6_1302213133/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tpmodul6_1302213133;
+
+SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("How to make a Red Velvet");
+sayaTubeVideo.IncreasePlayCount(50);
+sayaTubeVideo.PrintVideoDetail();
